09f2a2918ff64042ddd5035697ac9aeebad51974
Abdullah Irfan <abdullah.irfan@emumba.com>
2025-09-18 13:42:43 +0500

    Changed vector_bucket_name to collection_name

--- mem0/configs/vector_stores/s3_vectors.py
+++ mem0/configs/vector_stores/s3_vectors.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 
 class S3VectorsConfig(BaseModel):
-    vector_bucket_name: str = Field(description="Name of the S3 Vector bucket")
+    collection_name: str = Field(description="Name of the S3 Vector bucket")
     index_name: str = Field("mem0", description="Name of the vector index")
     embedding_model_dims: int = Field(
         1536, description="Dimension of the embedding vector"
